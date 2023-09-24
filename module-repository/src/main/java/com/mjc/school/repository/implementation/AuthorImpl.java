f87677e68e351c571b974a2ca4fbba04e7b98799
2110107 <2110107@newuzbekistanuniversity.uz>
2023-09-24 11:01:50 +0500

    done module-repository

---
+++ module-repository/src/main/java/com/mjc/school/repository/implementation/AuthorImpl.java
@@ -0,0 +1,9 @@
+package com.mjc.school.repository.implementation;
+
+public class AuthorImpl {
+    private String fileName;
+
+    public AuthorImpl(String fileName) {
+        this.fileName = fileName;
+    }
+}
